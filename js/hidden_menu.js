8977b7dd7358b81d4be05da261bb6fa7d9668b96
hyounji375 <hyounji375@gmail.com>
2022-07-03 19:10:13 +0900

    first

--- js/hidden_menu.js
+++ js/hidden_menu.js
@@ -1,18 +1,18 @@
-const bar = document.getElementById('bar')
-const hidemenu = document.querySelector('.hidemenu')
+const bar = document.getElementById("bar");
+const hidemenu = document.querySelector(".hide_menu");
 
-bar.addEventListener('click', () => {
-  if (hidemenu.classList.contains('on')) {
-    hidemenu.classList.remove('on')
+bar.addEventListener("click", () => {
+  if (hidemenu.classList.contains("on")) {
+    hidemenu.classList.remove("on");
   } else {
-    hidemenu.classList.add('on')
+    hidemenu.classList.add("on");
   }
-})
+});
 
-hidemenu.addEventListener('mouseleave', () => {
-  if (hidemenu.classList.contains('on')) {
-    hidemenu.classList.remove('on')
+hidemenu.addEventListener("mouseleave", () => {
+  if (hidemenu.classList.contains("on")) {
+    hidemenu.classList.remove("on");
   } else {
-    hidemenu.classList.add('on')
+    hidemenu.classList.add("on");
   }
-})
+});
